d2eae86b1b075e77363fdc66540c3ca38aa916bb
diego <diego@NP530U3C>
2017-10-31 22:03:00 -0200

    Criada classe Principal e OlaMundo!!

---
+++ br.cev.java.basico/src/br/cev/java/basico/OlaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cev.java.basico;
+
+/**
+ *
+ * @author diego
+ */
+public class OlaMundo {
+    
+    public static void olamundo() {
+        int a = 0;
+        while (a++ < 10) {
+        System.out.println("Ola Mundo!!!");
+        }
+}
+    
+}
